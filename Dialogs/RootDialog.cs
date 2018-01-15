1baac40de69a7c565fae61815de2027aca8ae7d8
Deep-09 <deepbhasme@gmail.com>
2018-01-15 17:19:54 +0530

    test

--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -17,7 +17,7 @@
 
         public async Task StartAsync(IDialogContext context)
         {
-            string data = context.UserData.ToString();
+            string data = context.Activity.ToString();
             await context.PostAsync(data);
             context.Wait(this.MessageReceivedAsync);
         }
